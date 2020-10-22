a9cb230ea5ee52eeaebdc058e92ef0be43e24988
Sampreet <sam.learnnew@gmail.com>
2020-10-22 15:15:33 -0700

    Basic integration for Playlist display and Add Form, needs works on Edit, Delete and Favorites

--- src/App.js
+++ src/App.js
@@ -1,104 +1,115 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js';
-import { Route, Link, Switch } from "react-router-dom";
-import Form from './Form'
-import Display from './Display'
-import {BrowserRouter as Router} from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
+import Form from './Form';
+import Playlist from './Playlist';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  const url = "https://tunr-backend-alex-t1.herokuapp.com/"
+	const url = 'https://tunr-backend-alex-t1.herokuapp.com';
 
-  const [songData, setSongData] = React.useState([])
+	const emptySong = {
+		title: '',
+		artist: '',
+		time: '',
+	};
+	const [songData, setSongData] = React.useState(emptySong);
 
-  const emptySong = {
-    title: "",
-    artist: "",
-    time: ""
-  };
+	const [selectedSong, setSelectedSong] = React.useState(emptySong);
 
-  const [selectedSong, setSelectedSong] = React.useState(emptySong)
+	//fetch songs
+	const getSongs = () => {
+		fetch(url + '/songs/')
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setSongData(data);
+			});
+	};
 
-  //fetch songs
-  const getSongs =() => {
-    fetch(url + "/songs/")
-    .then(response => response.json())
-    .then(data=>{
-      setSongData(data)
-    })
-  }
+	//get songs on page load
+	React.useEffect(() => {
+		getSongs();
+	}, []);
 
-  //get songs on page load
-  React.useEffect(() => {
-    getSongs()
-  }, [])
+	//create songs
+	const handleCreate = (newSong) => {
+		fetch(url + '/songs/', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newSong),
+		}).then((response) => getSongs());
+	};
 
-  //create songs
-  const handleCreate = (newSong) => {
-    fetch(url + "/songs/", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newSong)
-    })
-    .then(response => getSongs())
-  }
+	//update songs
+	const handleUpdate = (song) => {
+		fetch(url + '/songs/' + song._id, {
+			method: 'put',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(song),
+		}).then((response) => getSongs());
+	};
 
-  //update songs
-  const handleUpdate = (song) => {
-    fetch(url + "/songs/" + song._id, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(song)
-    })
-    .then(response => getSongs())
-  }
+	//select song
+	const selectSong = (song) => {
+		setSelectedSong(song);
+	};
 
-  //select song
-  const selectSong = (song) => {
-    setSelectedSong(song)
-  }
+	//delete song
+	const deleteSong = (song) => {
+		fetch((url = '/songs/' + song._id), {
+			method: 'delete',
+		}).then((response) => getSongs());
+	};
 
-  //delete song
-  const deleteSong = (song) => {
-    fetch(url ="/songs/" + song._id, {
-      method: "delete",
-    })
-    .then((response)=>getSongs())
-  }
-  
-  return (
-    <Router>
-    <div className="App">    
-     <Header />
-      <h3>ADD A NEW Song</h3>
-      <main>
-        <Switch>
-          <Route exact path="/" render={(rp) => (<Display {...rp} songs={songData} selectSong={selectSong} deleteSong={deleteSong}/>)} />
-          <Route
-            exact
-            path="/create"
-            render={(rp) => (
-              <Form {...rp} label="create" song={emptySong} handleSubmit={handleCreate} />
-            )}
-          />
-          <Route
-            exact
-            path="/edit"
-            render={(rp) => (
-              <Form {...rp} label="update" song={selectedSong} handleSubmit={handleUpdate} />
-            )}
-          />
-        </Switch>
-        <Link to="/create">
-        <button>ADD NEW SONG</button>
-      </Link>
-      </main>
-    </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Header />
+
+				<h3>ADD A NEW Song</h3>
+				<main>
+					<Switch>
+						<Route
+							exact
+							path='/'
+							render={(rp) => (
+								<div>
+									<Playlist
+										songs={songData}
+										deleteSong={deleteSong}
+										selectSong={selectSong}
+									/>
+									<Form
+										{...rp}
+										label='Add New Song'
+										song={emptySong}
+										handleSubmit={handleCreate}
+									/>
+								</div>
+							)}
+						/>
+						<Route
+							exact
+							path='/edit'
+							render={(rp) => (
+								<Form
+									{...rp}
+									label='Save'
+									song={selectedSong}
+									handleSubmit={handleUpdate}
+								/>
+							)}
+						/>
+					</Switch>
+				</main>
+			</div>
+		</Router>
+	);
 }
 export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,42 +1,50 @@
 import React from 'react';
 
 const Form = (props) => {
-    const [formData, setFormData] = React.useState(props.song);
+	const emptySong = {
+		title: '',
+		artist: '',
+		time: '',
+	};
+	const [formData, setFormData] = React.useState(props.song);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        props.handleSubmit(formData);
-        props.history.push('/');
-    };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		props.handleSubmit(formData);
+		setFormData(emptySong);
+		props.history.push('/');
+	};
 
-    const handleChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-    };
-    
-    return(
-    <form class="songForm" onSubmit={handleSubmit}>
-        <input 
-        id="title"
-        type="text"
-        name="title"
-        value={formData.title}
-        onChange={handleChange}/>
-        <input 
-        id="artist"
-        type="text"
-        name="artist"
-        value={formData.artist}
-        onChange={handleChange}/>
-        <input
-        id="time"
-        type="text"
-        name="time"
-        value={formData.time}
-        onChange={handleChange}/>
-        <input type="submit" value={props.label} />
-    </form>
-)
+	const handleChange = (event) => {
+		setFormData({ ...formData, [event.target.name]: event.target.value });
+	};
 
-}
+	return (
+		<form class='songForm' onSubmit={handleSubmit}>
+			<input
+				id='title'
+				type='text'
+				name='title'
+				value={formData.title}
+				onChange={handleChange}
+			/>
+			<input
+				id='artist'
+				type='text'
+				name='artist'
+				value={formData.artist}
+				onChange={handleChange}
+			/>
+			<input
+				id='time'
+				type='text'
+				name='time'
+				value={formData.time}
+				onChange={handleChange}
+			/>
+			<input type='submit' value={props.label} />
+		</form>
+	);
+};
 
 export default Form;
