76bcc0e15ce0c2a216036d2a891c24d5e9311f37
Shayan Gagan <shayangagan@Shayans-MacBook-Pro.local>
2020-10-22 14:32:23 -0500

    header is made and integrated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header.js';
+// import { Route, Link, Switch } from "react-router-dom";
+// import Display from "./Display";
 
 function App() {
   return (
-    <h3>THe App Component</h3>
+    <Header />
+    
   );
 }
 
---
+++ src/Display.js
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// const Display = (props) => {
+//     const { playlist } = props;
+
+//     const loaded = () => (
+//         <div style = {{ textAlign: "left" }}
+//         >
+//             {playlist.map(playlist => (
+//                 <
+//             ))}
+//         </div>
+//     )
+// }
+
+// export default Display;
